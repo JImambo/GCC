ea7f0d3ef416caa79cb0627bcc9af2516291d51b
JImambo <imambojonathan@gmail.com>
2025-06-02 10:41:53 +0000

    exo: Variables and types

---
+++ Variables_and_Types/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3 ;
+    float b = 4.5 ;
+    double c = 5.25 ;
+    float sum ;
+
+    printf ( "La somme de a, b et c est %f." , sum );
+    return 0;
+}
